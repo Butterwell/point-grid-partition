5247829e80c55ef6386efb54916c8276e706650f
Christopher Michael Buck <butterwell@gmail.com>
2022-07-29 09:46:38 -0400

    return indexes rather than points

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,9 @@ interface BBox {
 }
 
 interface Partitions {
+  shape: Array<number>,
   bBoxes: Array<BBox>,
-  grouped: Array<Array<Point>>
+  grouped: Array<Array<number>>
 }
 
 export function bBoxFromPoints(points: Array<Point>): BBox {
@@ -34,24 +35,23 @@ export function bBoxFromPoints(points: Array<Point>): BBox {
   }
 }
 
-// Moves the bBox rather than moving the points... either would work
 // Padding is split evenly so the orignial points are centered
-export function paddedPartitionedPoints(points: Array<Point>, originalBBox: BBox, padding: Point, partitionsPerAxis: number) : Partitions {
+export function padBBox(originalBBox: BBox, padding: Point) : BBox {
   const paddedBBox = {
     x: originalBBox.x - padding.x * 0.5,
     y: originalBBox.y - padding.y * 0.5,
     width: originalBBox.width + padding.x,
     height: originalBBox.height + padding.y
   }
-  const result = partitionedPoints(points, paddedBBox, partitionsPerAxis)
-  return result
+  return paddedBBox
 }
 
 export function partitionedPoints(points: Array<Point>, bBox: BBox, partitionsPerAxis: number) : Partitions {
   if (partitionsPerAxis <= 1) {
     return {
+      shape: [1, 1],
       bBoxes: [bBox],
-      grouped: [points]
+      grouped: [[...Array(points.length).keys()]]
     }
   }
 
@@ -60,7 +60,8 @@ export function partitionedPoints(points: Array<Point>, bBox: BBox, partitionsPe
   const step = size / partitionsPerAxis
 
   // Linear array of empty arrays of partitions*partions size
-  const grouped = Array.from(Array<Array<Point>>(partitionsPerAxis*partitionsPerAxis), () => []);
+  //const grouped = Array.from(Array<Array<Point>>(partitionsPerAxis*partitionsPerAxis), () => []);
+  const grouped = Array.from(Array<Array<number>>(partitionsPerAxis*partitionsPerAxis), () => []);
 
   points.forEach((p, i) => {
     let xx = Math.floor((p.x - bBox.x)/step)  // + halfStep?
@@ -68,13 +69,16 @@ export function partitionedPoints(points: Array<Point>, bBox: BBox, partitionsPe
     let yy = Math.floor((p.y - bBox.y)/step)  // + halfStep?
     let y = yy == partitionsPerAxis ? yy - 1 : yy
     let index = y*partitionsPerAxis + x
-    if (index > grouped.length) {
-      console.log(i, x, y)
-      console.log(p)
-      console.log(bBox)
-    }
     let point: Point = {x: p.x, y: p.y}
     grouped[index] = [...grouped[index], point]
+    // if (ies[index].length > 1) {
+    //   let last = ies[index].slice(-1)[0]
+    //   if (last+1 != i) {
+    //     console.log('out of sequence')
+    //     console.log(last, i)
+    //   }
+    // }
+    // ies[index] = [...ies[index], i]
   })
 
   const bBoxes = Array.from(Array<BBox>(partitionsPerAxis*partitionsPerAxis), (_, i) => {
@@ -85,7 +89,9 @@ export function partitionedPoints(points: Array<Point>, bBox: BBox, partitionsPe
     return { x, y, width, height }
   })
 
-  return { bBoxes, grouped }
+  const shape = [partitionsPerAxis, partitionsPerAxis]
+
+  return { shape, bBoxes, grouped }
 }
 
 function within(bBox: BBox, point: Point): boolean {
@@ -96,40 +102,44 @@ function within(bBox: BBox, point: Point): boolean {
   return true
 }
 
-export function withinBBox(bBox: BBox, points: Array<Point>): boolean {
-  let good = points.reduce((prev: boolean, point: Point) => within(bBox, point), true)
+export function withinBBox(bBox: BBox, points: Array<Point>, indexes: Array<number>): boolean {
+  let good = indexes.reduce((prev: boolean, index: number) => within(bBox, points[index]), true)
   return good
 }
 
 export function badlyPartitioned(points: Array<Point>, bBox: BBox, partitionsPerAxis: number) : Partitions {
   let partitioned = partitionedPoints(points, bBox, partitionsPerAxis)
   let bad = partitioned.bBoxes.reduce((prev: Partitions, bBox: BBox, i: number) => {
-    let points = partitioned.grouped[i]
-    let good = withinBBox(bBox, points)
+    let indexes = partitioned.grouped[i]
+    let good = withinBBox(bBox, points, indexes)
     if (good) return prev
     let result = {
       bBoxes: [...prev.bBoxes, bBox],
-      grouped: [...prev.grouped, points]
+      grouped: [...prev.grouped, indexes]
     }
     return result
   }, { bBoxes: [], grouped: []})
-  return bad
+  const shape = [partitionsPerAxis, partitionsPerAxis]
+  const bBoxes = bad.bBoxes
+  const grouped = bad.grouped 
+  return { shape, bBoxes, grouped }
 }
 
-function zeroOrThree(points: Array<Point>): Array<Point> {
-  if (points.length < 3) return []
-  const p1 = points[0]
-  const p2 = points[Math.floor(points.length/2)]
-  const p3 = points[points.length-1]
-  return [p1, p2, p3]
+function zeroOrThree(indexes: Array<number>): Array<number> {
+  if (indexes.length < 3) return []
+  const i1 = indexes[0]
+  const i2 = indexes[Math.floor(indexes.length/2)]
+  const i3 = indexes[indexes.length-1]
+  return [i1, i2, i3]
 }
 
 export function partitionedThreePoints(points: Array<Point>, bBox: BBox, partitionsPerAxis: number) : Partitions {
 
   const firstPass = partitionedPoints(points, bBox, partitionsPerAxis)
 
-  const grouped = firstPass.grouped.map((points) => zeroOrThree(points))
+  const shape = firstPass.shape
+  const grouped = firstPass.grouped.map((indexes) => zeroOrThree(indexes))
   const bBoxes = firstPass.bBoxes
   
-  return { bBoxes, grouped }
+  return { shape, bBoxes, grouped }
 }
